3c7f01351d3f4e4e6d6189184f2b96fe4823bca1
Perry Werneck <perry.werneck@gmail.com>
2022-12-13 07:25:08 -0300

    Fixing some CodeQL alerts.

--- src/module/private.h
+++ src/module/private.h
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+ #pragma once
+
  // https://stackoverflow.com/questions/9378593/dbuswatch-and-dbustimeout-examples
 
  #include <config.h>
@@ -35,10 +37,16 @@
 
 		class Alert : public Udjat::Abstract::Alert {
 		public:
+
 			/// @brief D-Bus message argument.
 			struct Argument {
 				int type;			///< @brief D-Bus data type.
 				String value;		///< @brief Argument value.
+
+				/// @brief Construct D-Bus argument from XML node.
+				/// @param parent Parent object.
+				/// @param group Group name.
+				/// @param node XML node for argument properties.
 				Argument(const Abstract::Object &parent, const char *group, const pugi::xml_node &node);
 			};
 
